8a5d99011144e49c0a59f650222865b5ecb29a11
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2024-10-03 12:33:38 +0500

    chapter three exercises

--- scripts/script.js
+++ scripts/script.js
@@ -93,3 +93,98 @@ for (let i = 0; i < GRID_SIZE; i++) {
 
     console.log(result);
 }
+
+
+/**
+ * 
+ * Minimum
+ * 
+ * The previous chapter introduced the standard function Math
+ * min that returns its smallest argument. We can write a
+ * function like that ourselves now. Define the function min
+ * that takes two arguments and returns their minimum.
+ */
+
+function findMinimum(a, b) {
+    return Math.min(a, b);
+}
+
+console.log(findMinimum(1, 3));
+console.log(findMinimum(-100, 100));
+
+/**
+ * 
+ * Recursion
+ * 
+ * We’ve seen that we can use % (the remainder operator) to
+ * test whether a number is even or odd by using % 2 to see
+ * whether it’s divisible by two. Here’s another way to define
+ * whether a positive whole number is even or odd:
+ * 
+ * Zero is even.
+ * One is odd.
+ * 
+ * For any other number N, its evenness is the same as N - 2.
+ * 
+ * Define a recursive function isEven corresponding to this
+ * description. The function should accept a single parameter
+ * (a positive, whole number) and return a Boolean.
+ * 
+ * Test it on 50 and 75. See how it behaves on -1. Why? Can
+ * you think of a way to fix this?
+ */
+
+function isEven(number) {
+    if(number == 0) return true;
+    else if(number == 1) return false;
+    else return isEven(number - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+
+
+console.log("____________________________________");
+
+/**
+ * 
+ * Write a function called countBs that takes a string as its
+ * only argument and returns a number that indicates how many
+ * uppercase B characters there are in the string.
+ * 
+ * Next, write a function called countChar that behaves like
+ * countBs, except it takes a second argument that indicates
+ * the character that is to be counted (rather than counting
+ * only uppercase B characters). Rewrite countBs to make use
+ * of this new function.
+ */
+
+function countBs(string) {
+    let upperCaseLetters = 0;
+    
+    for(let i = 0; i < string.length; i++) {
+        let character = string.charAt(i).toUpperCase();
+        if(character === string.charAt(i)) {
+            upperCaseLetters++;
+        }
+    }
+
+    return upperCaseLetters;
+}
+
+console.log(countBs("SARIM"));
+
+function countChar(string, character) {
+    let characterCounter = 0;
+
+    for(let i = 0; i < string.length; i++) {
+        if(character === string.charAt(i)) {
+            characterCounter++;
+        }
+    }
+
+    return characterCounter;
+}
+
+console.log(countChar("kdfkdjfdjfdhgi", "k"));
+
